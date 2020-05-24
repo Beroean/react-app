8632514d381e044cbda3ab9c4e2ed5b40adec634
Mahmoud Shaar <m.shaar94@gmail.com>
2020-05-24 18:30:42 -0500

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Mahmoud's Website. I'm going to update you.
+          Welcome to Mahmoud's Website. I'm going to update you again.
         </p>
         <a
           className="App-link"
