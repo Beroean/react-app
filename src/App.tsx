ac862b93f459ed3efda2be363a951b603b5fa569
Mahmoud Shaar <m.shaar94@gmail.com>
2020-05-24 17:51:30 -0500

    test rename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Mahmoud's Website. I'm going to delpoy you to Heroku
+          Welcome to Mahmoud's Website. I'm going to update you.
         </p>
         <a
           className="App-link"
