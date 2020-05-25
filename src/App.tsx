0bbc8967c6698fb96d900f03010f1d609fc91ee1
Mahmoud Shaar <m.shaar94@gmail.com>
2020-05-24 19:17:09 -0500

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {Home, Foo, Bar} from './components/pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Mahmoud's Website. I'm going to update you again.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+          <nav>
+          <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/foo">Foo</Link>
+          </li>
+          <li>
+            <Link to="/bar">Bar</Link>
+          </li>
+        </ul>
+
+        <hr />
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/foo" component={Foo} />
+            <Route exact path="/bar" component={Bar} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
