e69bd76c4776f7e4a76ffdd45a3e097ff4537be5
Mahmoud Shaar <m.shaar94@gmail.com>
2020-05-25 09:36:07 -0500

    Add tabs and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import {Home, Foo, Bar} from './components/pages';
+import React, { Fragment } from 'react';
+import { Route, Link, Switch, BrowserRouter } from 'react-router-dom';
+import { Home, Foo, Bar } from './components/pages';
 import './App.css';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 
 function App() {
-  return (
-    <Router>
-        <div>
-          <nav>
-          <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/foo">Foo</Link>
-          </li>
-          <li>
-            <Link to="/bar">Bar</Link>
-          </li>
-        </ul>
 
-        <hr />
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/foo" component={Foo} />
-            <Route exact path="/bar" component={Bar} />
-          </Switch>
-        </div>
-      </Router>
+  const allTabs = ['/', '/foo', '/bar'];
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs value={location.pathname} centered>
+                <Tab label="Home" value="/" component={Link} to={allTabs[0]} />
+                <Tab label="Foo" value="/foo" component={Link} to={allTabs[1]} />
+                <Tab
+                  label="Bar"
+                  value="/bar"
+                  component={Link}
+                  to={allTabs[2]}
+                />
+              </Tabs>
+              <Switch>
+                <Route exact path={allTabs[0]} component={Home} />
+                <Route path={allTabs[1]} component={Foo} />
+                <Route path={allTabs[2]} component={Bar} />
+              </Switch>
+            </Fragment>
+          )}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
