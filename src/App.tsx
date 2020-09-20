9fe5107e6adee63cd5e20187c6fb47e87fb477e1
Mahmoud Shaar <m.shaar94@gmail.com>
2020-09-20 12:48:16 -0500

    add box shadow

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const styles = (theme: Theme) =>
     app: {
       "& .MuiTabs-root": {
         backgroundColor: theme.palette.grey[900],
+        boxShadow: "0px 2px 4px -1px",
       },
     },
     flag: {
